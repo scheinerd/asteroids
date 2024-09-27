e9b2cd1b7ab9b380474c829acb6770a12d71d2b9
scheinerd <dscheiner93@gmail.com>
2024-09-27 15:48:22 +0200

    added two new files/classes to create and spawn asteroids

--- main.py
+++ main.py
@@ -1,33 +1,38 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
-    
-    updateable = pygame.sprite.Group()
-    drawable = pygame.sprite.Group()
 
-    # add all instances of a Player to updateable and drawable
-    Player.containers = (updateable, drawable)
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        
-        for object in updateable:
+
+        for object in updatable:
             object.update(dt)
 
-        screen.fill(pygame.Color(0,0,0))
+        screen.fill("black")
 
         for object in drawable:
             object.draw(screen)
@@ -36,6 +41,7 @@ def main():
 
         # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
